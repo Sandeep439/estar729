3ef4e7450081d9c436ef1f20b51694a096a27561
Sandeep439 <72335883+Sandeep439@users.noreply.github.com>
2021-07-27 15:36:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,103 +1,87 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <style>
-            body{
-                background-color: #89ABE3FF;
+            .nav{
+                position: absolute;
+                top:0%;
+                width:467px;
+                height: 50px;
+                background-color: aqua;
             }
-            #navbar{
-                background-color: #ef9273;
-            }
-            #links{
+            a{
                 color: black;
+                text-decoration: none;
             }
-            #links:hover{
-                color: #fef9f8;
+            .p{
+                background-color: aquamarine;
             }
-            #modal{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-35%, 100%);
-    }
-    #alert{
-    position: absolute;
-    top: auto;
-    left: 0%;
-    width: 100%;
-    background-color:#c8d8e4;
-    text-align: center;
-}
-    
+            .con{
+                display:grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                grid-template-rows: 1fr;
+            }
+            .b{
+                background-color: antiquewhite;
+                margin: 1em;
+                width: 300px;
+                height: 300px;
+            }
+            @media(max-width: 500px){
+                .nav{
+                position: absolute;
+                top:0%;
+                width:400px;
+                height: 50px;
+                background-color: aqua;
+                display: flex;
+                align-items: center;
+                font-weight: bold;
+                }
+                .p{
+                    position:  relative;
+                    top: 2em;
+                }
+                a{
+                    color: black;
+                    text-decoration: none;
+                }
+                a:hover{
+                    color: tomato;
+                }
+                .con{
+                    display:grid;
+                    grid-template-columns: 1fr;
+                    grid-template-rows: auto;
+                    background-color: azure;
+                    padding: 7px;
+                }
+                .b:hover{
+                    width: 307px;
+                    height: 307px;
+                    justify-content: center;
+                    
+                }
+                .b{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+            }
         </style>
     </head>
     <body>
-        <img src="home.jfif" alt="" class="img-fluid" style="position:fixed;top:0%;left:0%;width: auto;
-  height: auto;">
-        <!--NAVBAR-->
-
-        <nav class="navbar navbar-expand-sm navbar-light" id="navbar">
-            <div class="container-fluid" >
-              <a class="navbar-brand" href="#"><img src="logo.png" alt="" style="position: relative; height: 40px; width: 40px;">E Star</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown" >
-                <ul class="navbar-nav" id="linkcon">
-                  <li class="nav-item">
-                    <a class="nav-link"  href="#" id="links">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" id="links">Shop</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" id="links">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" id="links">Contact</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          <div class="alert alert-warning alert-dismissible fade show" role="alert" id="alert">
-            Opening Sale, 28% Discounts On Every Product!!!
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" ></button>
-            </div>
-          <h3 style="position: absolute;top:35%;left: 20%;color:#0d0d0d;font-family: 'Bona Nova', serif;font-size:30px;">New Products, Great Deals</h3>
-          <h4 style="position: absolute;top:45%;left:28%;color:#0d0d0d;">Limited Time Price Drop!</h4>
-          <button class="btn btn-primary" id="modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style="background-color:#ef9273;text-decoration:none;color:black;">Shop Now</button>
-   
-    <!-- Modal -->
-  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" >
-    <div class="modal-dialog" id="box">
-      <div class="modal-content" style="background-color: #c8d8e4">
-        <div class="modal-header" >
-        <img src="usericon.png" alt="" style="position:absolute;top:1%;left:44%;border-radius:50%;height: 70px;width:70px;">
-          <h5 class="modal-title" id="staticBackdropLabel" >Estar Login!!!</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          <br><br>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <nav><div class="nav">
+            <a href="#">Home</a>
+        </div></nav>
+        <div class="p"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero aspernatur, voluptate, eligendi eveniet amet quo eos nesciunt, nihil temporibus nam ipsam reiciendis iste accusantium? Temporibus provident vero cumque saepe non?</p>
         </div>
-        <div class="modal-body">
-            <form action="validate.php" method="POST">
-                <div class="form-floating mb-3">
-                  <input type="email" class="form-control" id="floatingInput" placeholder="Email" name="email">
-                  <label for="floatingInput">Email</label>
-                </div>
-                <div class="form-floating">
-                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="pwd">
-                  <label for="floatingPassword">Password</label>
-                </div>
-                <br>
-                <button class="btn btn-success" >Login</button>
-        </form>              
+        <br><br><br><br>
+        <div class="con">
+            <div class="b">1</div>
+            <div class="b">2</div>
+            <div class="b">3</div>
         </div>
-      </div>
-    </div>
-  </div>
-    
-    
-      </body>
+    </body>
 </html>
